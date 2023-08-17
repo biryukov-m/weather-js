89e39b32ab8998e77dde6d886df61aecb76bb60a
Maxym Biriukov <biryukov.mm@gmail.com>
2023-08-18 02:16:16 +0300

    ref: Fix forecast serialization every 8ths item

--- src/types/forecast.types.ts
+++ src/types/forecast.types.ts
@@ -58,17 +58,31 @@ interface City {
 }
 
 export const serializeForecast = (forecastApiResponse: IForecastResponse) => {
-  return forecastApiResponse.list.map(({ dt, main: dayMain, weather }) => ({
-    date: new Date(dt),
-    temp: dayMain.temp.toString(),
-    temp_min: dayMain.temp_min.toString(),
-    temp_max: dayMain.temp_max.toString(),
-    weather: weather.map(({ main, icon, description }) => ({
-      main,
-      icon,
-      description
-    }))
+  return forecastApiResponse.list
+    .filter((_item, idx) => idx % 8 === 0)
+    .map(({ dt, main: dayMain, weather }) => ({
+      date: new Date(dt * 1000),
+      temp: dayMain.temp.toString(),
+      temp_min: dayMain.temp_min.toString(),
+      temp_max: dayMain.temp_max.toString(),
+      weather: weather.map(({ main, icon, description }) => ({
+        main,
+        icon,
+        description
+      }))
+    }));
+};
+
+export const serializeForecastChart = (
+  forecastApiResponse: IForecastResponse
+) => {
+  return forecastApiResponse.list.slice(0, 9).map(({ dt, main }) => ({
+    date: new Date(dt * 1000).toLocaleTimeString(undefined, {
+      timeStyle: 'short'
+    }),
+    temp: main.temp
   }));
 };
 
 export type IForecast = ReturnType<typeof serializeForecast>;
+export type IForecastChart = ReturnType<typeof serializeForecastChart>;
